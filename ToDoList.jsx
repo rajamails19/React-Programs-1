61aea21fdf3c914d941cc7d4b559d0802a23e40b
Raja V <rajamails19@gmail.com>
2025-02-04 08:34:40 -0500

    Create ToDoList.jsx

---
+++ ToDoList.jsx
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+
+const TodoList = () => {
+  const [tasks, setTasks] = useState([]);
+  const [task, setTask] = useState('');
+
+  const addTask = () => {
+    if (task.trim()) {
+      setTasks([...tasks, task]);
+      setTask('');
+    }
+  };
+
+  return (
+    <div>
+      <h3>To-Do List</h3>
+      <input 
+        type="text" 
+        value={task} 
+        onChange={(e) => setTask(e.target.value)} 
+        placeholder="Add a task"
+      />
+      <button onClick={addTask}>Add</button>
+      <ul>
+        {tasks.map((t, index) => (
+          <li key={index}>{t}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default TodoList;
