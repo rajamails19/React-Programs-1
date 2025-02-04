6e24625aabda792711b302f42a8cd4319c86fcf6
Raja V <rajamails19@gmail.com>
2025-02-04 13:12:06 -0500

    Create BackgroundColorChanger.jsx

---
+++ BackgroundColorChanger.jsx
@@ -0,0 +1,18 @@
+import React, { useState } from 'react';
+
+const colors = ['red', 'blue', 'green', 'yellow', 'purple', 'orange'];
+
+const BackgroundChanger = () => {
+  const [bgColor, setBgColor] = useState('white');
+
+  return (
+    <div style={{ backgroundColor: bgColor, height: '100vh', textAlign: 'center', paddingTop: '20px' }}>
+      <h2>Click the button to change background color</h2>
+      <button onClick={() => setBgColor(colors[Math.floor(Math.random() * colors.length)])}>
+        Change Color
+      </button>
+    </div>
+  );
+};
+
+export default BackgroundChanger;
